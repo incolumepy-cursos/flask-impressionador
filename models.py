9d8fa5b95fab5224f31380392c31310deccde27d
Britodfbr <britodfbr@gmail.com>
2021-04-17 15:16:35 -0300

    Definido o modelo de dados para Post e Usuario

---
+++ models.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = '@britodfbr'
+
+from datetime import datetime
+from main import database
+
+
+class Usuario(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    password = database.Column(database.String, nullable=False)
+    foto = database.Column(database.String, default='default.png')
+    posts = database.relationship('Post', backref='author', lazy=True)
+    cursos = database.Column(database.String, nullable=False, default='Não Informado')
+
+
+class Post(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    title = database.Column(database.String, nullable=False)
+    message = database.Column(database.Text, nullable=False)
+    criated = database.Column(database.DateTime, nullable=False, default=datetime.utcnow)
+    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable=False)
